55da8bd6749ed8d04882d6276170e7ccb2ea29d2
shivanshunigam <shivanshunigam6@gmail.com>
2026-02-03 11:43:16 +0530

    deployment

--- day8/Backend/src/app.js
+++ day8/Backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express")
 
 const noteModel = require("./modules/note.model")
-
+const cors=require("cors")
+const path = require("path")
 
 const app = express();
+app.use(cors())
 app.use(express.json())
 
 app.post('/api/notes', async (req, res) => {
@@ -29,5 +31,20 @@ const noteid=await noteModel.findByIdAndDelete(id)
     req.status(200).json({
         message:"Note deleted succesfully "
     })
+})
+
+app.patch('/api/notes/:id',async(req,res)=>{
+    const id =req.params.id
+    const {description}=req.body
+   await noteModel.findByIdAndUpdate(id,{description})
+
+   res.status(200).json({
+    message:"note updated successfully"
+   })
+})
+console.log(_dirname)
+// middleware
+app.use('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
 })
 module.exports = app;
