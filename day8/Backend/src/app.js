f6c8ae72f9bf31314f4fa19ddad9f31eb1efb642
shivanshunigam <shivanshunigam6@gmail.com>
2026-02-04 11:17:11 +0530

    middleware

--- day8/Backend/src/app.js
+++ day8/Backend/src/app.js
@@ -7,6 +7,7 @@ const path = require("path")
 const app = express();
 app.use(cors())
 app.use(express.json())
+app.use(express.static("./public"))
 
 app.post('/api/notes', async (req, res) => {
     const { title, description } = req.body
